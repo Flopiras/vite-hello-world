62a24f16a18dff1563b6f1ee0c1be5b0a719ee6c
Flopiras <Flopiras@yahoo.it>
2023-06-28 14:55:39 +0200

    add AppJumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppJumbotron from './components/AppJumbotron.vue';
+
 export default {
   data() {
     return {
 
     }
   },
-  components: { AppTitle }
+  components: { AppTitle, AppJumbotron }
 }
 </script>
 
 <template>
   <!-- header -->
   <AppTitle />
+  <!-- jumbotron -->
+  <AppJumbotron />
 </template>
---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    data() {
+        return {
+            picName: 'darth'
+        }
+    },
+    methods: {
+        getImagePath(picName) {
+            const url = new URL(`../assets/${picName}.png`, import.meta.url)
+
+            return url.href;
+        }
+    }
+}
+</script>
+
+<template>
+    <jumbotron>
+        <img :src="getImagePath(picName)" alt="Darth Vader">
+    </jumbotron>
+</template>
+
+<style>
+img {
+    max-width: 1200px;
+    display: block;
+    object-fit: cover;
+}
+</style>
